74e8a543dd599b57343c09ea613a611486fbdfdb
Sohan <sosohan404@gmail.com>
2024-09-19 11:34:46 +0545

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +21,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <Toaster />
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
